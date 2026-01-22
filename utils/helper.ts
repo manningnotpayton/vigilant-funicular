32a404ee8bc9fe9f4f7c221a95ed074860c16f6d
manningnotpayton <manningnotpayton@users.noreply.github.com>
2026-01-22 17:37:00 +0200

    Fix concurrency bug

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,7 +1,12 @@
-def main():
-    print('Hello, World!')
-    data = {'key': 'value'}
-    return data
+function processData(data) {
+    if (!data) {
+        return null;
+    }
+    return data.map(item => ({
+        id: item.id,
+        name: item.name,
+        processed: true
+    }));
+}
 
-if __name__ == '__main__':
-    main()
+module.exports = { processData };
